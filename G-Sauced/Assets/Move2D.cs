797b0ffe7b3253c9e9ee90e5bfbacdfee1cc4979
Galen Nakama <gnakama17@punahou.edu>
2019-11-19 22:31:19 -0800

    part 1 of player combat, getting the sprite to be animated still need to get moving and attacking

---
+++ G-Sauced/Assets/Move2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+
+    public float moveSpeed = 100f;
+
+    float horizontalMove = 0f;
+
+    public Animator animator; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += movement * Time.deltaTime * moveSpeed;
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed; 
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+}
