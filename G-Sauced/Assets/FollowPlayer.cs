828a06347c794ec5c5f9f00065f46325863c1b68
Jihyunnie <43895453+Jihyunnie@users.noreply.github.com>
2019-11-22 06:32:54 -0800

    Revert "no message"
    This reverts commit 9dd2d53e0fd504d3be69100c7acb1d2788b99df9.

---
+++ G-Sauced/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player1;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player1.position + offset; 
+    }
+}
--- G-Sauced/Assets/PlayerAttack.cs
+++ G-Sauced/Assets/PlayerAttack.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+
+    public Animator animator; 
+
+    public Transform attackPos;
+    public float attackRange;
+
+    public LayerMask whatIsEnemies;
+
+    public int damage; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,27 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeBtwAttack <= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    animator.SetBool("isAttacking", true);
+                    enemiesToDamage[i].GetComponent<Enemy>().takeDamage(damage);  
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        } else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    public void OnDrawGizomosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange); 
     }
 }
