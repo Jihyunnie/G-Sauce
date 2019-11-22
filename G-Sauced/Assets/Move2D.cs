236573d685716615f6e43f57647c09e3dbf0ea1b
Galen Nakama <gnakama17@punahou.edu>
2019-11-21 23:01:25 -0800

    updates to player movement and player combat, still encountering an error "NullReferenceException" from the player's PlayerAttack script

--- G-Sauced/Assets/Move2D.cs
+++ G-Sauced/Assets/Move2D.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Move2D : MonoBehaviour
 {
 
+    public CharacterController2D controller; 
+
     public float moveSpeed = 100f;
 
+
+
     float horizontalMove = 0f;
+    bool jump = false; 
 
     public Animator animator; 
 
@@ -20,11 +25,22 @@ public class Move2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        //transform.position += movement * Time.deltaTime * moveSpeed;
+       // Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+       // transform.position += movement * Time.deltaTime * moveSpeed;
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed; 
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true; 
+        }
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false; 
     }
 }
--- G-Sauced/Assets/PlayerAttack.cs
+++ G-Sauced/Assets/PlayerAttack.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+
+    public LayerMask whatIsEnemies;
+
+    public int damage; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,26 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeBtwAttack <= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().takeDamage(damage);  
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        } else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizomosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange); 
     }
 }
